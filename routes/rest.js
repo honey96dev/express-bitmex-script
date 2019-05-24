4ef19ca2086b4f85be8377639541ddd7ee7c2e3f
Fullservice Fullstacked <honey96dev@gmail.com>
2019-05-24 03:11:25 +0300

    close position is also done

--- routes/rest.js
+++ routes/rest.js
@@ -32,6 +32,7 @@ router.post('/order', function (req, res, next) {
 
     const bitmexApi = new BitMEXApi(testnet, apiKeyID, apiKeySecret);
 
+    const orderID = order.orderID.length > 36 ? order.orderID.substr(order.orderID.length - 36, 36) : order.orderID;
     let data = {
         "symbol": order.symbol,
         "side": order.side,
@@ -48,9 +49,11 @@ router.post('/order', function (req, res, next) {
         "timeInForce": order.timeInForce,
         "execInst": order.execInst,
         "contingencyType": order.contingencyType,
-        "text": order.orderID,
+        // "text": order.orderID,
+        "text": orderID,
         // "text": order.text,
     };
+    console.log('post', orderID, orderID.length);
     bitmexApi.order(POST, data, (data) => {
         res.status(200).send(data);
     }, (error) => {
@@ -69,7 +72,7 @@ router.delete('/order', function (req, res, next) {
 
     const bitmexApi = new BitMEXApi(testnet, apiKeyID, apiKeySecret);
     const filter = JSON.stringify({
-        text: order.orderID
+        // text: '*' + order.orderID
     });
     console.log('rest-order delete', testnet, apiKeyID, apiKeySecret, filter);
     bitmexApi.order(GET, {filter: filter}, (data) => {
@@ -79,21 +82,48 @@ router.delete('/order', function (req, res, next) {
             res.status(200).send({});
             return;
         }
-        const item = data[0];
-        let body = {
-            "orderID": item.orderID,
-            // "clOrdID": item.clOrdID,
-            // "text": item.text,
-        };
-        bitmexApi.order(DELETE, body, (data) => {
-            res.status(200).send(data);
-        }, (error) => {
-            res.status(500).send(error);
-        });
+        for (let item of data) {
+            if (!item.text.endsWith(order.orderID)) continue;
+
+            // const item = data[0];
+            let body = {
+                "orderID": item.orderID,
+                // "clOrdID": item.clOrdID,
+                // "text": item.text,
+            };
+            bitmexApi.order(DELETE, body, (data) => {
+                res.status(200).send(data);
+            }, (error) => {
+                res.status(500).send(error);
+            });
+        }
     }, (error) => {
         res.status(500).send(error);
     });
+});
 
+router.delete('/orderAll', function (req, res, next) {
+    const headers = req.headers;
+    const params = req.body;
+
+    const testnet = Boolean(headers.testnet);
+    const apiKeyID = headers.apikeyid;
+    const apiKeySecret = headers.apikeysecret;
+    const symbol = params.symbol;
+    const filter = params.filter;
+    // const text = params.text;
+    const data = {
+        symbol: !!symbol ? symbol : '*',
+        filter: !!filter ? filter : '',
+    };
+
+    const bitmexApi = new BitMEXApi(testnet, apiKeyID, apiKeySecret);
+    console.log('rest-order delete', testnet, apiKeyID, apiKeySecret, filter);
+    bitmexApi.orderAll(DELETE, data, (data) => {
+        res.status(200).send(data);
+    }, (error) => {
+        res.status(500).send(error);
+    });
 });
 
 router.get('/position', function (req, res, next) {
@@ -115,3 +145,85 @@ router.get('/position', function (req, res, next) {
 });
 
 module.exports = router;
+
+//
+//
+// import express from 'express';
+// import {BitMEXApi, GET, POST, PUT, DELETE} from '../core/BitmexApi';
+//
+// const router = express.Router();
+//
+// router.get('/order', function (req, res, next) {
+//     const headers = req.headers;
+//     const params = req.query;
+//
+//     const testnet = Boolean(headers.testnet);
+//     const apiKeyID = headers.apikeyid;
+//     const apiKeySecret = headers.apikeysecret;
+//     const filter = params.filter;
+//     console.log('rest-order post', testnet, apiKeyID, apiKeySecret, filter);
+//     const bitmexApi = new BitMEXApi(testnet, apiKeyID, apiKeySecret);
+//     bitmexApi.order(GET, {filter: filter}, (data) => {
+//         res.status(200).send(data);
+//     }, (error) => {
+//         res.status(500).send(error);
+//     });
+// });
+//
+// router.post('/order', function (req, res, next) {
+//     const headers = req.headers;
+//     const params = req.body;
+//
+//     const testnet = Boolean(headers.testnet);
+//     const apiKeyID = headers.apikeyid;
+//     const apiKeySecret = headers.apikeysecret;
+//     const order = params.order;
+//     console.log('rest-order post', testnet, apiKeyID, apiKeySecret, JSON.stringify(order));
+//
+//     const bitmexApi = new BitMEXApi(testnet, apiKeyID, apiKeySecret);
+//
+//     let data = {
+//         "symbol": order.symbol,
+//         "side": order.side,
+//         "simpleOrderQty": order.simpleOrderQty,
+//         "orderQty": order.orderQty,
+//         "price": order.price,
+//         "displayQty": order.displayQty,
+//         "stopPx": order.stopPx,
+//         "clOrdID": order.clOrdID,
+//         "clOrdLinkID": order.clOrdLinkID,
+//         "pegOffsetValue": order.pegOffsetValue,
+//         "pegPriceType": order.pegPriceType,
+//         "ordType": order.ordType,
+//         "timeInForce": order.timeInForce,
+//         "execInst": order.execInst,
+//         "contingencyType": order.contingencyType,
+//         "text": order.orderID.startsWith('Spam') ? order.orderID.substr(order.orderID.length - 36, 36) : order.orderID,
+//         // "text": order.text,
+//     };
+//     bitmexApi.order(POST, data, (data) => {
+//         res.status(200).send(data);
+//     }, (error) => {
+//         res.status(500).send(error);
+//     });
+// });
+//
+// router.get('/position', function (req, res, next) {
+//     const headers = req.headers;
+//     const params = req.query;
+//
+//     const testnet = Boolean(headers.testnet);
+//     const apiKeyID = headers.apikeyid;
+//     const apiKeySecret = headers.apikeysecret;
+//     const filter = params.filter;
+//
+//     console.log('rest-position get', testnet, apiKeyID, apiKeySecret, filter);
+//     const bitmexApi = new BitMEXApi(testnet, apiKeyID, apiKeySecret);
+//     bitmexApi.position(GET, {filter: filter}, (data) => {
+//         res.status(200).send(data);
+//     }, (error) => {
+//         res.status(500).send(error);
+//     });
+// });
+//
+// module.exports = router;
