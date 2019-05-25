67ca8192c53760de722507e3d2e25878b40f5500
Fullservice Fullstacked <honey96dev@gmail.com>
2019-05-25 16:43:07 +0300

    now dashboard table is done with dummy content

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import config from '../core/config';
+
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index/index', {
+        baseUrl: config.server.baseUrl,
+        scripts: ['javascripts/pages/crud/metronic-datatable/advanced/row-details.js',],
+    });
+    // res.render('index/index', {baseUrl: config.server.baseUrl});
 });
 
 module.exports = router;
